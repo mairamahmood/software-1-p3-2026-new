1f4657d30d41f52e2f76c22ad38d78a6178d1532
maira <mairanasirmahmood@gmail.com>
2026-02-16 19:17:06 +0200

    testing exercise 5 module 2

---
+++ Module 3/exercise 1.py
@@ -0,0 +1,11 @@
+length = int(input("Enter the length of the zander in centimeters: "))
+
+size_limit = 42
+
+if length < size_limit:
+    difference = size_limit - length
+    print("The zander is too small.")
+    print(f"Release the fish back into the lake.")
+    print(f"It is {difference} cm below the size limit.")
+else:
+    print("The zander meets the size limit. You may keep it.")
---
+++ Module 3/exercise 4.py
@@ -0,0 +1,8 @@
+# Ask the user to enter a year
+year = int(input("Enter a year: "))
+
+# Check if the year is a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("The year is a leap year.")
+else:
+    print("The year is not a leap year.")
